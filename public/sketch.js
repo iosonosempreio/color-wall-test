06f32b9def1920647cf96bf1ba5a3dfcc84835b1
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-11-10 23:24:54 +0100

    init

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,64 @@
-function preload(){
+let myColor, size=2;
+const socket = io();
+socket.on("connect", newConnection);
+socket.on("color", (color) => {
+  console.log(color);
+  myColor = color;
+});
+socket.on("mouseBroadcast", otherMouse);
+
+function preload() {
   // put preload code here
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth, windowHeight);
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
-  background("lavender")
-  ellipse(mouseX, mouseY, 30);
+  // background("lavender");
+  if (myColor&&mouseIsPressed) {
+    push();
+    fill(myColor);
+    noStroke()
+    ellipse(mouseX, mouseY, size);
+    pop();
+    if (size<30) size++;
+  } else {
+    size=2
+  }
+}
+
+function newConnection() {
+  console.log("your id:", socket.id);
+}
+
+function mouseDragged() {
+  handleInteraction();
+  return false
+}
+function touchMoved() {
+  handleInteraction();
+  return false
+}
+
+function handleInteraction() {
+  let message = {
+    id: socket.id,
+    color: myColor,
+    size,
+    x: mouseX,
+    y: mouseY,
+  };
+  socket.emit("mouse", message);
+}
+function otherMouse(data) {
+  console.log("other mouse data", data);
+  push();
+  fill(data.color);
+  noStroke()
+  ellipse(data.x, data.y, data.size);
+  pop();
 }
--- server.js
+++ server.js
@@ -1,8 +1,37 @@
-let express = require("express")
-let port = 3000
-let app = express()
+const express = require("express")
+const socket = require('socket.io')
+
+const port = 3000
+const app = express()
 app.use(express.static('public'))
 
-let server = app.listen(port);
-
+const server = app.listen(port)
 console.log("I am up and running!")
+
+const io = socket(server);
+
+const newConnection = (socket) => {
+  const color = getRandomColor()
+  console.log('new connection:', socket.client.id, color)
+  socket.emit('color', color)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  const mouseMessage = (dataReceived) => {
+    console.log(dataReceived)
+    socket.broadcast.emit('mouseBroadcast', dataReceived)
+  }
+  socket.on('mouse', mouseMessage)
+}
+
+io.on('connection', newConnection)
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
