163fe4d93bc459b6c3e30c9dc2e81ae6cfb0632b
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-11-10 23:29:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const socket = require('socket.io')
 
-const port = 3000
+const port = process.env.PORT || 3000;
 const app = express()
 app.use(express.static('public'))
 
